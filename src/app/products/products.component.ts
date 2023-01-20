0000b84c804e420f5ebf2df488da07bd316aaa94
lamine1305 <lamine.mehidi01@gmail.com>
2023-01-20 19:57:29 +0100

    correction partie service

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductsService} from "../services/products.service";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-products',
@@ -8,17 +10,21 @@ import {Component, OnInit} from '@angular/core';
 export class ProductsComponent implements OnInit {
     products!: Array<any>;
 
-    constructor() {
+    productService: ProductsService;
+
+    constructor(private service: ProductsService) {
+        this.productService = service
     }
 
-  ngOnInit(): void {
-    this.products= [
-      {id:1, nom : "PC", Prix : 1000},
-      {id:2, nom : "MacBook", Prix : 1500},
-      {id:3, nom : "iPad", Prix : 1000},
-      {id:4, nom : "iPhone", Prix : 1200},
-      {id:5, nom : "Watch", Prix : 700},
-      {id:6, nom : "MacM2", Prix : 2000},
-    ];
-  }
+    ngOnInit(): void {
+        this.products = this.service.getAllProducts()
+    }
+
+    handleDeleteProduct(p: any) {
+        this.service.handleDeleteProduct(p);
+    }
+
+    handleReductionProduct(p: any) {
+        this.service.handleReductionProduct(p);
+    }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -8,17 +8,27 @@ export class ProductsService {
  private products!: Array<any>;
   constructor() { 
     this.products= [
-      {id:1, nom : "PC", Prix : 1000},
-      {id:2, nom : "MacBook", Prix : 1500},
-      {id:3, nom : "iPad", Prix : 1000},
-      {id:4, nom : "iPhone", Prix : 1200},
-      {id:5, nom : "Watch", Prix : 700},
-      {id:6, nom : "MacM2", Prix : 2000},
-      {id:6, nom : "MacM2", Prix : 2000},
+      {id:1, nom : "PC", prix : 1000},
+      {id:2, nom : "MacBook", prix : 1500},
+      {id:3, nom : "iPad", prix : 1000},
+      {id:4, nom : "iPhone", prix : 1200},
+      {id:5, nom : "Watch", prix : 700},
+      {id:6, nom : "MacM2", prix : 2000},
+      {id:6, nom : "MacM2", prix : 2000},
     ];
   }
 
-  public getAllProducts() : Observable<Array<any>>{
-    return of (this.products);
+  public getAllProducts() : Array<any>{
+    return this.products;
   }
+
+    handleDeleteProduct(p: any) {
+        let index = this.products.indexOf(p);
+        this.products.splice(index,1)
+    }
+
+    handleReductionProduct(p: any) {
+            p.prix = 0.5*p.prix
+        }
+
 }
