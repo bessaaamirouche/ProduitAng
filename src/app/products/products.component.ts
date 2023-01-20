c06a202697ca63046df4d775e59c5675bdab5123
lamine1305 <lamine.mehidi01@gmail.com>
2023-01-20 08:53:47 +0100

    Ajout de la partie solde

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-handleDeleteProduct(p: any) {
-let index = this.products.indexOf(p);
-this.products.splice(index,1)
-}
-  products! : Array<any>;
+    products!: Array<any>;
 
-  constructor() {}
+    constructor() {
+    }
 
-  ngOnInit(): void {
-    this.products= [
-      {id:1, nom : "PC", Prix : 1000},
-      {id:2, nom : "MacBook", Prix : 1500},
-      {id:3, nom : "iPad", Prix : 1000},
-      {id:4, nom : "iPhone", Prix : 1200},
-      {id:5, nom : "Watch", Prix : 700},
-      {id:6, nom : "MacM2", Prix : 2000},
-    ];
-  }
+    ngOnInit(): void {
+        this.products = [
+            {id: 1, nom: "PC", prix: 1000},
+            {id: 2, nom: "MacBook", prix: 1500},
+            {id: 3, nom: "iPad", prix: 1000},
+            {id: 4, nom: "iPhone", prix: 1200},
+            {id: 5, nom: "Watch", prix: 700},
+            {id: 6, nom: "MacM2", prix: 2000},
+        ];
+    }
+
+    handleDeleteProduct(p: any) {
+        let index = this.products.indexOf(p);
+        this.products.splice(index, 1)
+    }
+
+    handleReductionProduct(p: any) {
+        p.prix = 0.5*p.prix
+    }
 }
